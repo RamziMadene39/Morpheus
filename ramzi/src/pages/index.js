b87cc1745f84e6f69e6de23e5c317f58d495debd
RamziMadene39 <ramzi_madene@hotmail.com>
2022-01-02 20:13:54 +0300

    create gatsby site

---
+++ ramzi/src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+export default function IndexPage(){
+
+
+return(
+  <main>
+    <h1>Hello world</h1>
+  </main>
+)
+} 
+  
+
+git config --global user.email "ramzi_madene@hotmail.com"
+  git config --global user.name "RamziMadene39"
+
